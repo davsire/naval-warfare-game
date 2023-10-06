bdb25ea70c504b05e8734ea2d63f87113fc52f59
matheussteinbach <matheussteimbach4@gmail.com>
2023-10-06 12:44:45 -0300

    feat: adiciona aumenta_pontuacao_total

--- entidade/jogador.py
+++ entidade/jogador.py
@@ -10,60 +10,59 @@ class Jogador:
         self.__pontuacao_total = 0
         self.__jogos = []
 
-        @property
-        def id(self) -> int:
+    @property
+    def id(self) -> int:
             return self.__id
 
-        @property
-        def nome(self) -> str:
+    @property
+    def nome(self) -> str:
             return self.__nome
 
-        @property
-        def data_nascimento(self) -> str:
+    @property
+    def data_nascimento(self) -> str:
             return self.__data_nascimento
 
-        @property
-        def usuario(self) -> str:
+    @property
+    def usuario(self) -> str:
             return self.__usuario
 
-        @property
-        def senha(self) -> str:
+    @property
+    def senha(self) -> str:
             return self.__senha
 
-        @property
-        def pontuacao_total(self) -> int:
+    @property
+    def pontuacao_total(self) -> int:
             return self.__pontuacao_total
 
-        @property
-        def jogos(self) -> list:
+    @property
+    def jogos(self) -> list:
             return self.__jogos
 
-        @id.setter
-        def id(self, id: int):
+    @id.setter
+    def id(self, id: int):
             if isinstance(id, int):
                 self.__id = id
 
-        @nome.setter
-        def nome(self, nome: str):
+    @nome.setter
+    def nome(self, nome: str):
             if isinstance(nome, str):
                 self.__nome = nome
 
-        @data_nascimento.setter
-        def data_nascimento(self, data_nascimento: str):
+    @data_nascimento.setter
+    def data_nascimento(self, data_nascimento: str):
             if isinstance(data_nascimento, str):
                 self.__data_nascimento = data_nascimento
 
-        @usuario.setter
-        def usuario(self, usuario: str):
+    @usuario.setter
+    def usuario(self, usuario: str):
             if isinstance(usuario, str):
                 self.__usuario = usuario
 
-        @senha.setter
-        def senha(self, senha: str):
+    @senha.setter
+    def senha(self, senha: str):
             if isinstance(senha, str):
                 self.__senha = senha
 
-        @pontuacao_total.setter
-        def pontuacao_total(self, pontuacao_total: int):
-            if isinstance(pontuacao_total, int):
-                self.__pontuacao_total = pontuacao_total
+    def aumenta_pontuacao_total(self, pontuacao: int):
+        if isinstance(pontuacao, int):
+            self.__pontuacao_total += pontuacao
