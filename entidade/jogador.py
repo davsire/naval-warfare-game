faf059fd04c3c0f38ba0e5888fec94ba826c1eb2
matheussteinbach <matheussteimbach4@gmail.com>
2023-10-06 00:05:04 -0300

    docs: adiciona documentacao returns e setters

--- entidade/jogador.py
+++ entidade/jogador.py
@@ -11,59 +11,59 @@ class Jogador:
         self.__jogos = []
 
         @property
-        def id(self):
+        def id(self) -> int:
             return self.__id
 
         @property
-        def nome(self):
+        def nome(self) -> str:
             return self.__nome
 
         @property
-        def data_nascimento(self):
+        def data_nascimento(self) -> str:
             return self.__data_nascimento
 
         @property
-        def usuario(self):
+        def usuario(self) -> str:
             return self.__usuario
 
         @property
-        def senha(self):
+        def senha(self) -> str:
             return self.__senha
 
         @property
-        def pontuacao_total(self):
+        def pontuacao_total(self) -> int:
             return self.__pontuacao_total
 
         @property
-        def jogos(self):
+        def jogos(self) -> list:
             return self.__jogos
 
         @id.setter
-        def id(self, id):
+        def id(self, id: int):
             if isinstance(id, int):
                 self.__id = id
 
         @nome.setter
-        def nome(self, nome):
+        def nome(self, nome: str):
             if isinstance(nome, str):
                 self.__nome = nome
 
         @data_nascimento.setter
-        def data_nascimento(self, data_nascimento):
+        def data_nascimento(self, data_nascimento: str):
             if isinstance(data_nascimento, str):
                 self.__data_nascimento = data_nascimento
 
         @usuario.setter
-        def usuario(self, usuario):
+        def usuario(self, usuario: str):
             if isinstance(usuario, str):
                 self.__usuario = usuario
 
         @senha.setter
-        def senha(self, senha):
+        def senha(self, senha: str):
             if isinstance(senha, str):
                 self.__senha = senha
 
         @pontuacao_total.setter
-        def pontuacao_total(self, pontuacao_total):
-            if isinstance(pontuacao_total, id):
+        def pontuacao_total(self, pontuacao_total: int):
+            if isinstance(pontuacao_total, int):
                 self.__pontuacao_total = pontuacao_total
