45a1c5db254182ec289049d8f52f838cb460621e
matheussteinbach <matheussteimbach4@gmail.com>
2023-10-06 13:07:58 -0300

    fix: indentacao

--- entidade/jogador.py
+++ entidade/jogador.py
@@ -12,56 +12,56 @@ class Jogador:
 
     @property
     def id(self) -> int:
-            return self.__id
+        return self.__id
 
     @property
     def nome(self) -> str:
-            return self.__nome
+        return self.__nome
 
     @property
     def data_nascimento(self) -> str:
-            return self.__data_nascimento
+        return self.__data_nascimento
 
     @property
     def usuario(self) -> str:
-            return self.__usuario
+        return self.__usuario
 
     @property
     def senha(self) -> str:
-            return self.__senha
+        return self.__senha
 
     @property
     def pontuacao_total(self) -> int:
-            return self.__pontuacao_total
+        return self.__pontuacao_total
 
     @property
     def jogos(self) -> list:
-            return self.__jogos
+        return self.__jogos
 
     @id.setter
     def id(self, id: int):
-            if isinstance(id, int):
-                self.__id = id
+        if isinstance(id, int):
+            self.__id = id
 
     @nome.setter
     def nome(self, nome: str):
-            if isinstance(nome, str):
-                self.__nome = nome
+        if isinstance(nome, str):
+            self.__nome = nome
 
     @data_nascimento.setter
     def data_nascimento(self, data_nascimento: str):
-            if isinstance(data_nascimento, str):
-                self.__data_nascimento = data_nascimento
+        if isinstance(data_nascimento, str):
+            self.__data_nascimento = data_nascimento
 
     @usuario.setter
     def usuario(self, usuario: str):
-            if isinstance(usuario, str):
-                self.__usuario = usuario
+        if isinstance(usuario, str):
+            self.__usuario = usuario
 
     @senha.setter
     def senha(self, senha: str):
-            if isinstance(senha, str):
-                self.__senha = senha
+        if isinstance(senha, str):
+            self.__senha = senha
 
     def aumenta_pontuacao_total(self, pontuacao: int):
         if isinstance(pontuacao, int):
