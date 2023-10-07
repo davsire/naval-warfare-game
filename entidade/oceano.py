f2df57cf7c54cebf16c9ea81d4a1f86832309d38
Davi Vitorino da Silva <davi.silva@softplan.com.br>
2023-10-07 10:16:08 -0300

    feat: adiciona getter de tamanho do oceano e move métodos privados para o final da clsse

--- entidade/oceano.py
+++ entidade/oceano.py
@@ -13,8 +13,29 @@ class Oceano:
         self.__mapa = [[None for _ in range(tamanho)] for _ in range(tamanho)]
 
     @property
+    def tamanho(self):
+        return self.__tamanho
+
+    @property
     def mapa(self):
         return self.__mapa
+
+    def adicionar_embarcacao(self,
+                             sigla_embarcacao: str,
+                             coord_inicio: tuple,
+                             coord_final: tuple):
+        embarcacao = self.__criar_embarcacao(sigla_embarcacao)
+
+        self.__verificar_tamanho_embarcacao(embarcacao,
+                                            coord_inicio,
+                                            coord_final)
+        self.__verificar_conflito_embarcacao(coord_inicio, coord_final)
+
+        x_inicio, y_inicio = coord_inicio
+        x_final, y_final = coord_final
+        for linha in range(x_inicio, x_final + 1):
+            for coluna in range(y_inicio, y_final + 1):
+                self.__mapa[linha][coluna] = embarcacao
 
     def __criar_embarcacao(self, sigla_embarcacao: str) -> Embarcacao:
         lista_embarcacoes = {
@@ -45,20 +66,3 @@ class Oceano:
             for coluna in range(y_inicio, y_final + 1):
                 if isinstance(self.__mapa[linha][coluna], Embarcacao):
                     raise ConflitoEmbarcacaoErro(linha, coluna)
-
-    def adicionar_embarcacao(self,
-                             sigla_embarcacao: str,
-                             coord_inicio: tuple,
-                             coord_final: tuple):
-        embarcacao = self.__criar_embarcacao(sigla_embarcacao)
-
-        self.__verificar_tamanho_embarcacao(embarcacao,
-                                            coord_inicio,
-                                            coord_final)
-        self.__verificar_conflito_embarcacao(coord_inicio, coord_final)
-
-        x_inicio, y_inicio = coord_inicio
-        x_final, y_final = coord_final
-        for linha in range(x_inicio, x_final + 1):
-            for coluna in range(y_inicio, y_final + 1):
-                self.__mapa[linha][coluna] = embarcacao
